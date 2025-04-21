08c5e38589bcad248ca7d9990f173030ee792f66
Nguyen Trong Vy <trongvy1312@gmail.com>
2025-04-22 01:05:03 +0700

    rename fragment recipe deti+ail -> recipe_detail_admin

--- src/main/java/com/example/formular_cookie/RecipeDetailFragment.java
+++ src/main/java/com/example/formular_cookie/RecipeDetailFragment.java
@@ -41,7 +41,7 @@ public class RecipeDetailFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_recipe_detail, container, false);
+        return inflater.inflate(R.layout.fragment_recipe_detail_admin, container, false);
     }
 
     @Override
